ae4f49506d1c216c6fbd3a1d61402abef4144a68
Jacob M <31823746+Itsmillertyme@users.noreply.github.com>
2024-08-22 18:50:07 -0400

    Init commit
    First commit with base project

---
+++ TGH_MageGame/Assets/Scripts/TestScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+    // Start is called before the first frame update
+    void Awake() {
+        Debug.Log("THIS IS A TEST SCRIPT");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
