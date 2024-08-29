9c18af5d2090413df3354946e1dbe25b9eacf4f5
Jacob M <31823746+Itsmillertyme@users.noreply.github.com>
2024-08-28 20:32:55 -0400

    First commit with base project

---
+++ TGH_MageGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+/*
+ * Author: Jacob M.
+ * Date Created:8/27/2024
+ * Version: V1
+ */
+public class PlayerController : MonoBehaviour {
+
+    //Component references
+    PlayerInput playerInput;
+    CharacterController characterController;
+    InputAction moveAction;
+    Animator animator;
+    [SerializeField] GameObject playerModel; //SII
+
+    //Animations variables
+    int velocityHash;
+    //
+    //Vector2 animationBlendVector; //Current animation blend vector
+    //Vector2 animationVelocity;
+    //float animationSmoothTime = 0.1f;
+
+    //Player Properties
+    float velocity;
+    [SerializeField] float speed; //SII
+    [SerializeField] float acceleration; //SII
+    [SerializeField] float deceleration; //SII
+
+    // Start is called before the first frame update
+    void Awake() {
+
+        //Cache References
+        playerInput = GetComponent<PlayerInput>();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        //Cache animator strings as hashes
+        velocityHash = Animator.StringToHash("Velocity");
+
+        //Initialize actions
+        moveAction = playerInput.actions["Move"];
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Move();
+
+        //do gravity
+
+    }
+
+    void Move() {
+
+        //Read values from inputController for movement
+        float movementInput = moveAction.ReadValue<float>();
+        float animationInput = -movementInput;
+
+        //Process animation based on inputs
+        if (animationInput > 0.05 && velocity < 1) {
+            velocity += acceleration * animationInput * Time.deltaTime;
+        }
+        if (animationInput < 0.05 && velocity > 0) {
+            velocity -= deceleration * Time.deltaTime;
+        }
+        if (animationInput < 0.05 && velocity < 0) {
+            velocity = 0;
+        }
+
+        //set animator parameters
+        animator.SetFloat(velocityHash, velocity);
+
+        //scale velocity
+        float playerSpeed;
+        if (velocity < .6) {
+            playerSpeed = (-speed / 2) * velocity;
+        }
+        else {
+            playerSpeed = -speed * velocity;
+        }
+
+        //manually reset player model position
+        playerModel.transform.localPosition = Vector3.zero;
+        playerModel.transform.localRotation = Quaternion.Euler(new Vector3(0, -90, 0));
+
+        //setup actual movement vector
+        Vector3 movement = new Vector3(playerSpeed, 0, 0);
+
+        //move character
+        characterController.Move(movement);
+    }
+}
